cafe7228cd29fbdde35a2460a06b9e2c475a817c
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2021-06-06 09:01:42 +0200

    Wayland platform: comment out presently unused member functions

--- src/drivers/Wayland/Fl_Wayland_Screen_Driver.cxx
+++ src/drivers/Wayland/Fl_Wayland_Screen_Driver.cxx
@@ -1378,7 +1378,7 @@ void Fl_Wayland_Screen_Driver::reset_cursor() {
   xc_arrow = xc_ns = xc_wait = xc_insert = xc_hand = xc_help = xc_cross = xc_move = xc_north = xc_south = xc_west = xc_east = xc_we = xc_nesw = xc_nwse = xc_sw = xc_se = xc_ne = xc_nw = NULL;
 }
 
-uint32_t Fl_Wayland_Screen_Driver::get_serial() {
+/*uint32_t Fl_Wayland_Screen_Driver::get_serial() {
   return seat->serial;
 }
 
@@ -1386,7 +1386,7 @@ struct wl_seat*Fl_Wayland_Screen_Driver::get_wl_seat() {
   return seat->wl_seat;
 }
 
-/*char *Fl_Wayland_Screen_Driver::get_seat_name() {
+char *Fl_Wayland_Screen_Driver::get_seat_name() {
   return seat->name;
 }*/
 
