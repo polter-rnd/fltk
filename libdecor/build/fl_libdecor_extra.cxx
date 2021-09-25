1ffd8dba0d7634ecb4a7ae1d88e1fe5573f265e1
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2021-09-25 17:40:00 +0200

    Wayland platform: this file is no longer useful

--- libdecor/build/fl_libdecor_extra.cxx
+++
@@ -1,7 +0,0 @@
-#include "../../src/drivers/Wayland/Fl_Wayland_Screen_Driver.H"
-
-extern "C" {
-  bool fl_libdecor_using_weston(void) {
-    return Fl_Wayland_Screen_Driver::compositor == Fl_Wayland_Screen_Driver::WESTON;
-  };
-}
