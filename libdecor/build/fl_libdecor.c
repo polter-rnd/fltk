3e604427654bd8039d181c32c2bb646a6d207da9
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2021-09-26 09:42:01 +0200

    Wayland platform: restore minimisation of libdecor's demo

--- libdecor/build/fl_libdecor.c
+++ libdecor/build/fl_libdecor.c
@@ -3,15 +3,18 @@
 #undef libdecor_frame_set_minimized
 
 extern bool fl_libdecor_using_weston(void);
+//#include <stdio.h>
 
 LIBDECOR_EXPORT void libdecor_frame_set_minimized(struct libdecor_frame *frame)
 {
   static bool done = false;
   static bool using_weston = false;
   if (!done) {
+    typedef bool (*ext_f)(void);
+    volatile ext_f ext = fl_libdecor_using_weston;
     done = true;
-    using_weston = fl_libdecor_using_weston();
-//fprintf(stderr, "fl_libdecor_using_weston=%p using_weston=%d\n",fl_libdecor_using_weston,using_weston);
+    if (ext) using_weston = fl_libdecor_using_weston();
+//fprintf(stderr, "fl_libdecor_using_weston=%p using_weston=%d\n", fl_libdecor_using_weston, using_weston);
   }
   if (using_weston) libdecor_frame_set_visibility(frame, false);
   libdecor_frame_set_minimized_orig(frame);
