e3f4335fc04859d55064954eb9572b64c1a73440
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2021-09-28 17:24:30 +0200

    Wayland platform: fix bug in libdecor_frame_unref() of libdecor.c This statement     wl_list_remove(&frame->link); is missing in function libdecor_frame_unref().

--- libdecor/build/fl_libdecor.c
+++ libdecor/build/fl_libdecor.c
@@ -1,6 +1,8 @@
 #define libdecor_frame_set_minimized libdecor_frame_set_minimized_orig
+#define libdecor_frame_unref libdecor_frame_unref_orig
 #include "../src/libdecor.c"
 #undef libdecor_frame_set_minimized
+#undef libdecor_frame_unref
 
 extern bool fl_libdecor_using_weston(void);
 //#include <stdio.h>
@@ -18,4 +20,14 @@ LIBDECOR_EXPORT void libdecor_frame_set_minimized(struct libdecor_frame *frame)
   }
   if (using_weston) libdecor_frame_set_visibility(frame, false);
   libdecor_frame_set_minimized_orig(frame);
+}
+
+
+LIBDECOR_EXPORT void libdecor_frame_unref(struct libdecor_frame *frame)
+{
+  struct libdecor_frame_private *frame_priv = frame->priv;
+  if (frame_priv->ref_count == 1) {
+    wl_list_remove(&frame->link);
+  }
+  libdecor_frame_unref_orig(frame);
 }
