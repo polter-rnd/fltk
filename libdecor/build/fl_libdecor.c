89106cb7ec86a020974fdb5e891a1b1e35be0a96
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2021-10-21 14:26:46 +0200

    Wayland platform: update libdecor from source.
    It's no longer necessary to repair libdecor_frame_unref().

--- libdecor/build/fl_libdecor.c
+++ libdecor/build/fl_libdecor.c
@@ -1,8 +1,6 @@
 #define libdecor_frame_set_minimized libdecor_frame_set_minimized_orig
-#define libdecor_frame_unref libdecor_frame_unref_orig
 #include "../src/libdecor.c"
 #undef libdecor_frame_set_minimized
-#undef libdecor_frame_unref
 
 extern bool fl_libdecor_using_weston(void);
 //#include <stdio.h>
@@ -20,11 +18,4 @@ LIBDECOR_EXPORT void libdecor_frame_set_minimized(struct libdecor_frame *frame)
   }
   if (using_weston) libdecor_frame_set_visibility(frame, false);
   libdecor_frame_set_minimized_orig(frame);
-}
-
-
-LIBDECOR_EXPORT void libdecor_frame_unref(struct libdecor_frame *frame)
-{
-  if (frame->priv->ref_count == 1) wl_list_remove(&frame->link);
-  libdecor_frame_unref_orig(frame);
 }
