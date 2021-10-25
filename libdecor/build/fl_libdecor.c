7eac0f0916acc093495eb66521ec7c0dad141967
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2021-10-25 18:22:13 +0200

    Wayland platform: put libdecor code in lib/libfltk_decor.a instead of lib/libdecor-0.1.so. Statically linked FLTK apps for the wayland platform no longer depend on a shared library holding libdecor.

--- libdecor/build/fl_libdecor.c
+++ libdecor/build/fl_libdecor.c
@@ -1,6 +1,8 @@
 #define libdecor_frame_set_minimized libdecor_frame_set_minimized_orig
+#define libdecor_new libdecor_new_orig
 #include "../src/libdecor.c"
 #undef libdecor_frame_set_minimized
+#undef libdecor_new
 
 extern bool fl_libdecor_using_weston(void);
 //#include <stdio.h>
@@ -19,3 +21,30 @@ LIBDECOR_EXPORT void libdecor_frame_set_minimized(struct libdecor_frame *frame)
   if (using_weston) libdecor_frame_set_visibility(frame, false);
   libdecor_frame_set_minimized_orig(frame);
 }
+
+// defined in libdecor-cairo.c
+extern const struct libdecor_plugin_description libdecor_plugin_description;
+
+/*
+ FLTK modifies libdecor's libdecor_new() function so it uses the plugin implemented in libdecor-cairo.c
+ to decorate windows. No shared library is searched. No shared library is necessary.
+ */
+LIBDECOR_EXPORT struct libdecor *libdecor_new(struct wl_display *wl_display, struct libdecor_interface *iface)
+{
+  struct libdecor *context;
+  context = zalloc(sizeof *context);
+  context->ref_count = 1;
+  context->iface = iface;
+  context->wl_display = wl_display;
+  context->wl_registry = wl_display_get_registry(wl_display);
+  wl_registry_add_listener(context->wl_registry, &registry_listener, context);
+  context->init_callback = wl_display_sync(context->wl_display);
+  wl_callback_add_listener(context->init_callback, &init_wl_display_callback_listener, context);
+  wl_list_init(&context->frames);
+
+  context->plugin = libdecor_plugin_description.constructor(context);
+
+  wl_display_flush(wl_display);
+  return context;
+}
+
